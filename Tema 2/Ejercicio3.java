b5adf032dbe1a45b21e48d09347b108db2f5a417
Jose Daniel Jimenez Quintana <danieljimenezquintana@gmail.com>
2018-09-26 08:44:04 +0200

    Actualizado

--- Tema 2/Ejercicio3.java
+++ Tema 2/Ejercicio3.java
@@ -3,9 +3,9 @@ public class Ejercicio3 {
 	String nombre;
 	nombre = "Jose Daniel Jiménez Quintana";
 	String direccion;
-	direccion = "Manuel Freire 1";
+	direccion = "c/calle 1";
 	String telefono;
-	telefono = "658584286";
+	telefono = "658589304";
 	System.out.println( nombre );
 	System.out.println( direccion );
 	System.out.println( telefono );
