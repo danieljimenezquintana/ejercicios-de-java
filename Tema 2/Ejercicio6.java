f7180d87a2fb25b6fb3087b3855f076c7685ab54
Jose Daniel Jiménez Quintana <danieljimenezquintana@gmail.com>
2018-09-27 21:03:34 +0200

    Actualizando ejercicios

--- Tema 2/Ejercicio6.java
+++ Tema 2/Ejercicio6.java
@@ -1,9 +1,11 @@
 public class Ejercicio6 {
 	public static void main(String[] args) {
 	double iva;
-	iva = 1,21;
+	iva = 1.21;
 	double precio;
+	precio = 1.0;
 	double total;
 	total = precio * iva;
+	System.out.print("El total es: " + total);
 	}
 }
