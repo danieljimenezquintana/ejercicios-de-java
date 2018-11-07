00ab187b8ff7f9c3f58f7758796f2afe5077d073
Jose Daniel Jimenez Quintana <danieljimenezquintana@gmail.com>
2018-11-07 08:49:47 +0100

    Actualizado

--- Tema 7/Ejercicio5.java
+++ Tema 7/Ejercicio5.java
@@ -8,24 +8,30 @@ import java.util.Scanner;
  */
 public class Ejercicio5 {
   public static void main(String[] args){
-    int maximo = 0;
+    
     Scanner s = new Scanner(System.in);
     int[] numIntro;
     numIntro = new int[10];
     int x = 0;
-    int minimo = 2147483647;
+    
     System.out.println("Introduzca diez numeros:");
     for (int i = 0; i < 10; i++){
-      numIntro[x] = Integer.parseInt(s.nextLine()) ;
-      if (numIntro[x] > maximo){
-        maximo = numIntro[x];
-      }
-      if (numIntro[x] < minimo){
-        minimo = numIntro[x];
-      }
-      x++;
+      numIntro[i] = Integer.parseInt(s.nextLine()) ;
     }
-    System.out.println("El máximo es: " + maximo);
+    int maximo = numIntro[0];
+    int minimo = numIntro[0];
+      
+    for (int i = 0; i < 10; i++){
+        if (numIntro[x] > maximo){
+          maximo = numIntro[x];
+        }
+        if (numIntro[x] < minimo){
+          minimo = numIntro[x];
+        }
+        x++;
+      }
+    
+      System.out.println("El máximo es: " + maximo);
     System.out.println("El mínimo es: " + minimo);
   }
 }
