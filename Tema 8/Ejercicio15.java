93d43560687f43640ee9f411551347c8de2c6cbb
Jose Daniel Jimenez Quintana <danieljimenezquintana@gmail.com>
2018-11-27 12:21:50 +0100

    Actualizando

---
+++ Tema 8/Ejercicio15.java
@@ -0,0 +1,24 @@
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio15 {
+  public static void main(String[] args){
+  
+  for(int i = 1;i < 1001;i++){
+    if(Ejercicio1_14.esPrimo(i)){
+      System.out.println(i);
+    }
+  }
+  
+  }
+}
